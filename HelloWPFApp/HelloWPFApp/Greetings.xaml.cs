6c0a342cc9bce5f750519febaa252bf6fc16b2bd
Sheik Soegriemsingh <s.soegriemsingh@gmail.com>
2018-05-01 13:48:34 +0200

    Create a simple application with Visual C# tutorial

---
+++ HelloWPFApp/HelloWPFApp/Greetings.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+
+namespace HelloWPFApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            String content;
+
+            if (Button1.IsChecked == true)
+            {
+                content = Button1.Content.ToString();
+            }
+            else if (Button2.IsChecked == true)
+            {
+                content = Button2.Content.ToString();
+            }
+            else
+            {
+                ShowContent("No button selected");
+                return;
+            }
+
+            ShowContent(content);   
+        }
+
+        private void ShowContent(String content)
+        {
+            MessageBox.Show(content);
+        }
+    }
+}
